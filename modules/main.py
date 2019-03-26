2f64baa1255eb37d35587fde4afeecba54e1dc31
sambapython@gmail.com <sambapython@gmail.com>
2019-03-26 09:21:41 +0530

    2019-03-26 modules.

---
+++ modules/main.py
@@ -0,0 +1,7 @@
+import sales
+sales.create_customer()
+print(sales.a)
+print(sales.b)
+print(sales.c)
+res=sales.add(10,20)
+print(res)
---
+++ modules/sales.py
@@ -0,0 +1,9 @@
+a=1000
+b=2000
+c=a+b
+def create_customer():
+	print("this is customer creation..")
+def add(x,y):
+	return x+y
+
+#create_customer()
