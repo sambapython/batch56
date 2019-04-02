07d3994f7bd8502c3bf03aafaec2e77561397cb9
sambapython@gmail.com <sambapython@gmail.com>
2019-04-02 07:59:32 +0530

    2019-04-02 modules

--- modules/main.py
+++ modules/main.py
@@ -1,7 +1,32 @@
+'''
+import sys
+sys.path.append("/home/khyaathipython/")
+print(sys.path)
 import sales
+import pur
+'''
+'''
 sales.create_customer()
 print(sales.a)
 print(sales.b)
 print(sales.c)
 res=sales.add(10,20)
 print(res)
+'''
+'''
+import mod1
+'''
+'''
+from mod1 import file1,file2
+file1.fun()
+file2.fun()
+'''
+'''
+import mod1
+mod1.file1.fun()
+'''
+'''
+from mod1.mod1_1 import in_1
+in_1.fun()
+'''
+# if __name__ == "__main__"
---
+++ modules/mod1/mod1_1/in_1.py
@@ -0,0 +1,2 @@
+def fun():
+	print("this is fun in in_1")
